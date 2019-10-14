183a4afc5670c06d1376453303d7603bb6aded25
Boa Matule <boa.matule@gmail.com>
2019-10-14 21:39:56 +0200

    Modifies the feature displayEmployeeList to include the length of the employees.

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -1,10 +1,11 @@
-import { italic } from "ansi-colors"
-
 describe('Display list of employees', () => {
 })
 
-it('when user visits the page',() => {
+it('when user visits the page', () => {
   cy.visit('http://localhost:3000')
-  cy.get('section[name="header"]')
-    .should('contain', 'Employee list')
+  cy.get('section[name="main"]').within(() => {
+    cy.get('li')
+    .should('have.length', 5)
+  })
 })
+
