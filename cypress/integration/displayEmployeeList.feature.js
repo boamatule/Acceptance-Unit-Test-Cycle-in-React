f75cb3a3fa0c2868c42f7e0e53aa71da911cbe31
Boa Matule <boa.matule@gmail.com>
2019-10-14 21:02:02 +0200

    Creates a test in file in the cypress/integration folder; First test green.

---
+++ cypress/integration/displayEmployeeList.feature.js
@@ -0,0 +1,10 @@
+import { italic } from "ansi-colors"
+
+describe('Display list of employees', () => {
+})
+
+it('when user visits the page',() => {
+  cy.visit('http://localhost:3000')
+  cy.get('section[name="header"]')
+    .should('contain', 'Employee list')
+})
